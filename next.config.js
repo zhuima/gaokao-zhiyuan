3be795ddc465032351e7f448cc1009571395fe24
zhuima <zhuima314@gmail.com>
2023-06-26 18:54:06 +0800

    change the ipaddress

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 // https://zenn.dev/duo3/articles/dbb8115309059e
-import { setDefaultResultOrder } from 'dns'
-setDefaultResultOrder('ipv4first')
 
 const nextConfig = {
   images: {
