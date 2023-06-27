a3e6b678dc5792032e5ea8513717575ac3af6df8
zhuima <zhuima314@gmail.com>
2023-06-27 12:03:48 +0800

    production api

--- utils/endpoint.js
+++ utils/endpoint.js
@@ -1,2 +1,2 @@
-// export const endpoint = 'https://gkzy.ruankao.eu.org/api'
-export const endpoint = 'http://127.0.0.1:3000/api'
+export const endpoint = 'https://gkzy.ruankao.eu.org/api'
+// export const endpoint = 'http://127.0.0.1:3000/api'
