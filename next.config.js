690049cc498f83a32b65b413aa690b803b9e6fb7
zhuima <zhuima314@gmail.com>
2023-06-28 15:56:53 +0800

    add sitemap

--- next.config.js
+++ next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
