16e6a8601617264c78435463b865c323305081a7
zhuima <zhuima314@gmail.com>
2023-07-03 16:33:49 +0800

    change search button status

--- components/SearchForm.jsx
+++ components/SearchForm.jsx
@@ -21,24 +21,37 @@ export default function SearchForm() {
     <>
       <div className="mt-10 flex justify-center items-center">
         <div className="container mx-auto bg-indigo-500 rounded-lg p-14">
-          <form onSubmit={filterUniversity}>
+          <form onSubmit={filterUniversity} className="group">
             <h1 className="text-center font-bold text-white text-2xl tracking-tight text-slate-900 sm:text-5xl">
               快来搜一搜你心仪的大学
             </h1>
 
-            <div className="mt-10 sm:flex items-center bg-white rounded-lg overflow-hidden px-2 py-3 justify-between">
+            <div className="flex mt-10 sm:flex items-center bg-white rounded-lg overflow-hidden px-2 py-3 justify-between">
               <label htmlFor="name"></label>
               <input
-                className="text-base text-gray-400 flex-grow outline-none px-2 "
+                className="text-base text-gray-400 flex-grow outline-none px-2 focus:border-gray-500 focus:outline-none valid:[&:not(:placeholder-shown)]:border-green-500 [&:not(:placeholder-shown):not(:focus):invalid~span]:block invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-400"
                 id="name"
                 name="name"
                 type="text"
                 placeholder="根据大学名称或关键字检索"
+                autocomplete="off"
+                required
               />
-
-              {/* <button className="bg-indigo-500 text-white text-base rounded-lg px-4 py-2 font-thin">
+              {/* <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="根据大学名称或关键字检索"
+                class="w-full rounded-md border border-gray-300 px-3 py-2.5 placeholder-gray-300 shadow shadow-gray-100 focus:border-gray-500 focus:outline-none valid:[&:not(:placeholder-shown)]:border-green-500 [&:not(:placeholder-shown):not(:focus):invalid~span]:block invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-400"
+                autocomplete="off"
+                required
+              /> */}
+              <span className="mt-2 hidden text-sm text-red-400">
+                请输入有效的关键字或高校名称.{' '}
+              </span>
+              <button className="bg-indigo-500 text-white text-base rounded-lg px-4 py-2 font-thin focus:bg-indigo-600 focus:outline-none group-invalid:pointer-events-none group-invalid:opacity-70">
                 搜索
-              </button> */}
+              </button>
             </div>
           </form>
         </div>
