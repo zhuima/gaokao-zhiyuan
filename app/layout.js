eb12b26f06eed2add320eb2509ad69fd87f45525
zhuima <zhuima314@gmail.com>
2023-07-04 16:26:06 +0800

    change reading bar

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ import NextTopLoader from 'nextjs-toploader'
 import { Inter } from 'next/font/google'
 import { Navigation } from '@/components'
 import ScrollToTop from '@/components/ScrollToTopButton'
-import ReadingBar from '@/Components/ReadingBar'
+import ReadingBar from '@/components/ReadingBar'
 
 import './globals.css'
 
